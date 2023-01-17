7adf8075f24596d3ecf6f12e29ea1882bd0d3f78
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-01-17 20:13:38 +0300

    feat: add currency exchange rate page

--- src/pages/CurrencyExchangeRate.jsx
+++ src/pages/CurrencyExchangeRate.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const CurrencyExchangeRate = () => {
-  return <div>CurrencyExchangeRate</div>;
+  return (
+    <section className="currency-exchange-rate">
+      <h2>Currency Exchange Rate</h2>
+      <div>
+        <h3>Selected currency</h3>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>...</li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default CurrencyExchangeRate;
