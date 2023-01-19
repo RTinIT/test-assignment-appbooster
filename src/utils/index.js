67c84e579ca2e956cb00d4fd09deaf38c79a605b
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-01-19 15:54:50 +0300

    feat: add util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,17 @@
 export const getKey = (obj, value) => {
   return Object.keys(obj).find((key) => obj[key] === value);
 };
+
+export const round = (value) => {
+  if (typeof value === "string") {
+    const num = +value;
+    return num.toFixed(2);
+  }
+  console.log(value);
+  return value.toFixed(2);
+};
+
+export const getValue = (str, num = 1) => {
+  const arr = str.split(" ");
+  return arr[num];
+};
