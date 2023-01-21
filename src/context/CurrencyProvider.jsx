deb82b9b06964569ab2ecdaa4cb6c2605ded07f2
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-01-21 20:07:22 +0300

    feat: add response state about currency and handle

--- src/context/CurrencyProvider.jsx
+++ src/context/CurrencyProvider.jsx
@@ -9,6 +9,18 @@ export const useCurrency = () => useContext(CurrencyContext);
 export const CurrencyProvider = ({ children }) => {
   const [input, setInput] = useState("15 usd in rub");
   const [allCurrenciesCodes] = useFetch(getUrl());
+  const [result, setResult] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const [amount, selectedCurrency, , targetCurrency] = input.split(" ");
+
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${selectedCurrency}/${targetCurrency}.json`
+    )
+      .then((data) => data.json())
+      .then((data) => setResult(amount * data[targetCurrency]));
+  };
 
   const setCurrency = (value) => {
     setInput(value);
@@ -18,7 +30,9 @@ export const CurrencyProvider = ({ children }) => {
     <CurrencyContext.Provider
       value={{
         input,
+        result,
         setCurrency,
+        handleSubmit,
         allCurrenciesCodes,
       }}
     >
