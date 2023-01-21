a4be9cf05b7fd1322cafcf094bc865318e4f0363
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-01-21 20:10:50 +0300

    refactor: add styled components and split the code

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,26 +1,27 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import Button from "./Button";
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 2em;
+`;
 
 const Navigation = () => {
   return (
-    <nav className="navigation">
-      <div className="nav-item">
-        <NavLink
-          to="/test-assignment-appbooster/converter"
-          className={({ isActive }) => (isActive ? "active-page" : "menu-item")}
-        >
-          Converter
-        </NavLink>
-      </div>
-      <div className="nav-item">
-        <NavLink
-          to="/test-assignment-appbooster/currencies-exchange-rate"
-          className={({ isActive }) => (isActive ? "active-page" : "menu-item")}
-        >
-          All currencies
-        </NavLink>
-      </div>
-    </nav>
+    <Nav className="navigation">
+      <Button isLink={true} to="/test-assignment-appbooster/converter">
+        Converter
+      </Button>
+      <Button
+        isLink={true}
+        to="/test-assignment-appbooster/currencies-exchange-rate"
+      >
+        All currencies
+      </Button>
+    </Nav>
   );
 };
 
