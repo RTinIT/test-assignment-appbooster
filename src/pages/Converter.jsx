09967f2aa0292d9950c999e6d55e9d4beec4e5de
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-01-21 19:58:20 +0300

    refactor: add styled components, split on pieces

--- src/pages/Converter.jsx
+++ src/pages/Converter.jsx
@@ -1,59 +1,36 @@
-import React, { useState } from "react";
-import { useCurrency } from "../context/CurrencyProvider";
+import React from "react";
+import styled from "styled-components";
+import Search from "../components/Search";
+import useSearch from "../hooks/useSearch";
+import ConverterField from "../components/ConverterField";
+import OutputInfoField from "../components/OutputInfoField";
+
+const StyledSection = styled.main`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex-wrap: wrap;
+`;
 
 const Converter = () => {
-  const { input, setCurrency, allCurrenciesCodes } = useCurrency();
-  const [result, setResult] = useState("");
-  const [showList, setShowList] = useState(false);
-
-  const handle = (event) => {
-    event.preventDefault();
-    const [amount, selectedCurrency, , targetCurrency] = input.split(" ");
-
-    fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${selectedCurrency}/${targetCurrency}.json`
-    )
-      .then((data) => data.json())
-      .then((data) => setResult(amount * data[targetCurrency]));
-  };
+  const { search, message, saveSearch, handleSearch, searchResult } =
+    useSearch();
 
   return (
-    <section className="converter">
-      <h1>Currency Converter</h1>
-      <button onClick={() => setShowList((active) => !active)}>
-        Currencies
-      </button>
-      <div className="content" style={{ position: "relative" }}>
-        <aside className={showList ? "active" : ""}>
-          <ul>
-            {allCurrenciesCodes &&
-              Object.values(allCurrenciesCodes).map((c, i) => (
-                <li
-                  key={i}
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    width: "350px",
-                    borderBottom: "1px solid #000",
-                    padding: "2px 0",
-                  }}
-                >
-                  <span>{c}</span>
-                  <span>{Object.keys(allCurrenciesCodes)[i]}</span>
-                </li>
-              ))}
-          </ul>
-        </aside>
-
-        <form onSubmit={handle}>
-          <input value={input} onChange={(e) => setCurrency(e.target.value)} />
-          <p>Result:</p>
-          <input value={result} readOnly />
-          <button>Get result</button>
-        </form>
-      </div>
-    </section>
+    <StyledSection>
+      <Search
+        search={search}
+        setSearch={saveSearch}
+        message={message}
+        handleSearch={handleSearch}
+      />
+      <ConverterField />
+      {searchResult.length ? (
+        <OutputInfoField searchResult={searchResult} />
+      ) : (
+        ""
+      )}
+    </StyledSection>
   );
 };
 
