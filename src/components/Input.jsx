3182cea6fe50584ee540b868252f576faf894874
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-01-21 20:02:44 +0300

    feat: add component Input

---
+++ src/components/Input.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  width: 100%;
+  max-width: 220px;
+  min-width: 5em;
+  height: 20px;
+  padding: 12px;
+  border-radius: 4px;
+  border: 1.5px solid lightgrey;
+  outline: none;
+  font-size: 1em;
+  transition: all 0.3s cubic-bezier(0.19, 1, 0.22, 1);
+  box-shadow: 0px 0px 20px -18px;
+
+  &:active {
+    transform: scale(0.95);
+  }
+
+  &:focus {
+    outline: 2px solid grey;
+  }
+`;
+
+const Input = (props) => {
+  return <StyledInput {...props} />;
+};
+
+export default Input;
