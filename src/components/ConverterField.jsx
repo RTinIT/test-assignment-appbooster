fe6c8b9582cd84700c1ed1ca6164b8da99e8f543
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-01-21 20:01:08 +0300

    feat: add component ConverterField

---
+++ src/components/ConverterField.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+import Form from "./Form";
+import Input from "./Input";
+import Button from "./Button";
+import { useCurrency } from "../context/CurrencyProvider";
+
+const StyledConverter = styled.section`
+  flex: 3;
+  padding: 50px 20px;
+  border-radius: 4px;
+  background: #f5f5f5;
+`;
+
+const ConverterField = () => {
+  const { input, setCurrency, result, handleSubmit } = useCurrency();
+
+  return (
+    <StyledConverter>
+      <h1>Currency Converter</h1>
+      <Form onSubmit={handleSubmit}>
+        <Input value={input} onChange={(e) => setCurrency(e.target.value)} />
+        <p>Result:</p>
+        <Input value={result} readOnly />
+        <Button>Send</Button>
+      </Form>
+    </StyledConverter>
+  );
+};
+
+export default ConverterField;
