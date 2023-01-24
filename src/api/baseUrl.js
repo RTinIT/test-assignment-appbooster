32854699d1ea9e7a89eeedae93c791baeeab9d98
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-01-24 22:17:32 +0300

    docs: add API description

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,2 +1,19 @@
+/**
+ *  Free currency API - https://github.com/fawazahmed0/currency-api
+ *
+ *  Lists all the available currencies in prettified json format:
+ *  https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json
+ *
+ *  Get the currency list with EUR as base currency:
+ *  https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur.json
+ *
+ *  Get the currency list with EUR as base currency on date 2022-11-24:
+ *  https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/2022-11-24/currencies/eur.json
+ *
+ *  Get the currency value for EUR to JPY:
+ *  https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json
+ *
+ * */
+
 export const baseUrl =
   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
