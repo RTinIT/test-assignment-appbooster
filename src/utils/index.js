4050c74c5b5ad1cb85d73be54b3429cd2c2c0ed2
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-02-17 18:31:25 +0300

    feat: add functions for handle date

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,25 @@
-export const getKey = (obj, value) => {
+const getKey = (obj, value) => {
   return Object.keys(obj).find((key) => obj[key] === value);
 };
 
-export const getValue = (str, num = 1) => {
+const getValue = (str, num = 1) => {
   const arr = str.split(" ");
   return arr[num];
 };
 
-export const ifIsEmptySetMessage = (value, setMessage, message) => {
+const addZero = (v) => {
+  return +v < 10 ? "0" + v : v;
+};
+
+const getDate = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = addZero(date.getMonth() + 1);
+  const day = addZero(date.getDate());
+  return `${year}-${month}-${day}`;
+};
+
+const ifIsEmptySetMessage = (value, setMessage, message) => {
   if (!value) {
     setMessage(message);
     return 0;
@@ -16,3 +28,5 @@ export const ifIsEmptySetMessage = (value, setMessage, message) => {
     return 1;
   }
 };
+
+export { getKey, getValue, ifIsEmptySetMessage, getDate };
