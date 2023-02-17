cf4dbc25ea9776f06849d6e1545df6538b331904
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-02-17 18:31:59 +0300

    feat: add date to base url

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -15,5 +15,6 @@
  *
  * */
 
-export const baseUrl =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+import { getDate } from "../utils";
+
+export const baseUrl = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/${getDate()}/currencies`;
